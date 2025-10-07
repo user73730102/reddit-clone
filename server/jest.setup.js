99b8e9236add2727c499632aa95573f762ec4fa4
user73730102 <xa.varadvaidya36@gmail.com>
2025-10-07 23:07:36 +0530

    ci cd

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,3 +1,4 @@
 // This file runs before any server-side tests.
 // Its job is to load the environment variables from the .env file.
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
